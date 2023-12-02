3da5df650a44752ba30dcee610a7860da41e0357
247511466@qq.com <247511466@qq.com>
2023-12-02 22:56:44 +0800

    增加bigo开屏

--- pro/ios/app/OneTenDemo/OneTenDemo/AdSource/OTBigoAdapter.m
+++ pro/ios/app/OneTenDemo/OneTenDemo/AdSource/OTBigoAdapter.m
@@ -7,8 +7,10 @@
 
 #import "OTBigoAdapter.h"
 #import <BigoADS/BigoRewardVideoAdLoader.h>
+#import <BigoADS/BigoSplashAdLoader.h>
 
-@interface OTBigoAdapter ()<BigoRewardVideoAdLoaderDelegate, BigoRewardVideoAdInteractionDelegate>
+@interface OTBigoAdapter ()<BigoRewardVideoAdLoaderDelegate, BigoRewardVideoAdInteractionDelegate,
+                            BigoSplashAdLoaderDelegate, BigoSplashAdInteractionDelegate>
 
 @end
 
@@ -21,7 +23,10 @@
         }
             break;
         case OTAdSourceStyleTypeSplash: {
-            
+            if ([self.delegate.adSourceObject isKindOfClass:[BigoSplashAd class]]) {
+//                return [(BigoSplashAd *)self.delegate.adSourceObject isExpired];
+                return YES;
+            }
         }
             break;
         case OTAdSourceStyleTypeRewardedVideo: {
@@ -62,10 +67,10 @@
 - (void)registerWithUserInfo:(NSDictionary<id,id> *)userInfo {
     //init
     BigoAdConfig *adConfig = [[BigoAdConfig alloc] initWithAppId:@"10048891"];
-//    adConfig.age = 18;
-//    adConfig.gender = BigoAdGenderMale;
-//    adConfig.activatedTime = 12345678;
-//    adConfig.testMode = YES;
+    adConfig.age = 18;
+    adConfig.gender = BigoAdGenderMale;
+    adConfig.activatedTime = 12345678;
+    adConfig.testMode = YES;
 
     [[BigoAdSdk sharedInstance] initializeSdkWithAdConfig:adConfig completion:^{
 //        NSString *bidderToken = [[BigoAdSdk sharedInstance] getBidderToken];
@@ -83,7 +88,9 @@
         }
             break;
         case OTAdSourceStyleTypeSplash: {
-            
+            if ([self.delegate.adSourceObject isKindOfClass:[BigoSplashAd class]]) {
+                [(BigoSplashAd *)self.delegate.adSourceObject showInAdContainer:viewController.view];
+            }
         }
             break;
         case OTAdSourceStyleTypeRewardedVideo: {
@@ -102,12 +109,26 @@
     }
 }
 
+#pragma mark - Load
 /// start load rewarded video ad
 /// @param type c2s s2s
 /// @param userInfo info
 - (void)loadRewardedVideoWithType:(OTAdSourceType)type userInfo:(NSDictionary<id, id> *)userInfo {
     BigoRewardVideoAdRequest *request = [[BigoRewardVideoAdRequest alloc] initWithSlotId:@"10048891-10194136"];
     BigoRewardVideoAdLoader *adLoader = [[BigoRewardVideoAdLoader alloc] initWithRewardVideoAdLoaderDelegate:self];
+    [adLoader loadAd:request];
+    
+    if (self.delegate && [self.delegate respondsToSelector:@selector(adWillLoadWithStyleType:adSourceObject:)]) {
+        [self.delegate adWillLoadWithStyleType:OTAdSourceStyleTypeRewardedVideo adSourceObject:adLoader];
+    }
+}
+
+/// start load splash ad
+/// @param type c2s s2s
+/// @param userInfo info
+- (void)loadSplashWithType:(OTAdSourceType)type userInfo:(NSDictionary<id, id> *)userInfo {
+    BigoSplashAdRequest *request = [[BigoSplashAdRequest alloc] initWithSlotId:@"10048891-10064376"];
+    BigoSplashAdLoader *adLoader = [[BigoSplashAdLoader alloc] initWithSplashAdLoaderDelegate:self];
     [adLoader loadAd:request];
     
     if (self.delegate && [self.delegate respondsToSelector:@selector(adWillLoadWithStyleType:adSourceObject:)]) {
@@ -174,6 +195,44 @@
 //激励视频激励回调
 - (void)onAdRewarded:(BigoRewardVideoAd *)ad {
     
+}
+
+#pragma mark - Splash
+- (void)onSplashAdLoaded:(BigoSplashAd *)ad {
+    BOOL isReady = ad.isExpired;
+    if (self.delegate && [self.delegate respondsToSelector:@selector(adSaveSourceObject:)]) {
+        [self.delegate adSaveSourceObject:ad];
+    }
+    
+    [ad setSplashAdInteractionDelegate:self];
+    if (self.delegate && [self.delegate respondsToSelector:@selector(adDidLoadWithStyleType:error:)]) {
+        [self.delegate adDidLoadWithStyleType:OTAdSourceStyleTypeRewardedVideo error:nil];
+    }
+}
+
+- (void)onSplashAdLoadError:(BigoAdError *)bigoAdError {
+    if (self.delegate && [self.delegate respondsToSelector:@selector(adDidLoadWithStyleType:error:)]) {
+        NSError *error = [NSError errorWithDomain:bigoAdError.errorMsg code:bigoAdError.errorCode userInfo:nil];
+        [self.delegate adDidLoadWithStyleType:OTAdSourceStyleTypeRewardedVideo error:error];
+    }
+}
+
+/**
+ * 广告可跳过回调，通常是由用户点击了右上角 SKIP 按钮所触发
+ */
+- (void)onAdSkipped:(BigoAd *)ad {
+    if (self.delegate && [self.delegate respondsToSelector:@selector(adDidCloseWithStyleType:error:)]) {
+        [self.delegate adDidCloseWithStyleType:OTAdSourceStyleTypeRewardedVideo error:nil];
+    }
+}
+
+/**
+ * 广告倒计时结束回调
+ */
+- (void)onAdFinished:(BigoAd *)ad {
+    if (self.delegate && [self.delegate respondsToSelector:@selector(adDidCloseWithStyleType:error:)]) {
+        [self.delegate adDidCloseWithStyleType:OTAdSourceStyleTypeRewardedVideo error:nil];
+    }
 }
 
 @end
